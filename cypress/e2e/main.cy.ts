76d37c69cff60d965cdc6e4dc069dc0d7cb9be20
Henry <hnguyhenry@protonmail.com>
2023-08-28 15:07:21 -0700

    Add tests for different viewports

--- cypress/e2e/main.cy.ts
+++ cypress/e2e/main.cy.ts
@@ -1,4 +1,4 @@
-describe('Bookmark Tests', { retries: { runMode: 4, openMode: 1 } }, () => {
+describe('Bookmark Tests', { retries: 5 }, () => {
   beforeEach(() => {
     cy.visit('/');
   });
@@ -18,5 +18,18 @@ describe('Bookmark Tests', { retries: { runMode: 4, openMode: 1 } }, () => {
     // Click close button and make sure it's closed
     cy.get('[data-test="menu-close"').click();
     cy.get('@menu').should('be.hidden');
+
+    // Make sure it stays hidden when desktop view is present
+    cy.get('[data-test="menu-open"').click();
+    cy.viewport(1000, 660);
+    cy.get('@menu').should('be.hidden');
+    cy.viewport('iphone-8');
+    cy.get('@menu').should('not.be.hidden');
+    cy.get('[data-test="menu-close"').click();
+
+    // Make sure desktop anchor links are present
+    cy.get('[data-test="desktop-nav-items"]').should('not.be.visible');
+    cy.viewport(1000, 660);
+    cy.get('[data-test="desktop-nav-items"]').should('be.visible');
   });
 });
