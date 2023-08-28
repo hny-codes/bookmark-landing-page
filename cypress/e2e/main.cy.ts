4d3dd2e0aafc237aaae5b4daefd0640ffe172505
Henry <hnguyhenry@protonmail.com>
2023-08-28 14:40:52 -0700

    Add tests for nav bar functionality

--- cypress/e2e/main.cy.ts
+++ cypress/e2e/main.cy.ts
@@ -1,5 +1,22 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('Bookmark Tests', { retries: { runMode: 4, openMode: 1 } }, () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('Test navigation bar', () => {
+    // Set viewport
+    cy.viewport('iphone-8');
+
+    // Menu should be closed by default
+    cy.get('[data-test="menu"]').as('menu');
+    cy.get('@menu').should('be.hidden');
+
+    // Click menu and make sure it's visible
+    cy.get('[data-test="menu-open"').click();
+    cy.get('@menu').should('not.be.hidden');
+
+    // Click close button and make sure it's closed
+    cy.get('[data-test="menu-close"').click();
+    cy.get('@menu').should('be.hidden');
+  });
+});
