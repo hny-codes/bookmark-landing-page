0dc058dfc3b9e11f38458fa0a2515577d3582513
Henry <hnguyhenry@protonmail.com>
2023-09-04 00:52:15 -0700

    Adjust pseudo span bg to fit different viewports

--- src/components/TabFeature.tsx
+++ src/components/TabFeature.tsx
@@ -8,10 +8,12 @@ export default function TabFeature() {
   return (
     <Tabs.Root
       defaultValue={tabValue}
-      className='p-8 sm:text-left'
+      className='p-8 md:text-left relative'
       onValueChange={(value) => setTabValue(value)}
     >
-      <Tabs.List className='flex flex-col sm:flex-row sm:justify-evenly sm:w-2/3 sm:mx-auto gap-6 sm:gap-0 mb-14 sm:mb-16 [&>*]:py-2'>
+      <span className='illus'></span>
+
+      <Tabs.List className='flex flex-col sm:flex-row sm:justify-evenly sm:w-2/3 sm:mx-auto gap-6 sm:gap-0 mb-14 sm:mb-16 [&>*]:py-2 md:max-w-[--max-w]'>
         <Tabs.Trigger
           value='simple'
           className='relative sm:w-full'
@@ -52,18 +54,18 @@ export default function TabFeature() {
       <Tabs.Content
         value='simple'
         data-test='tab-1-content'
-        className='sm:grid sm:grid-cols-2  sm:content-wrapper'
+        className='md:grid md:grid-cols-2 md:content-wrapper'
       >
-        <div className='relative'>
+        <div className='relative pb-4 md:pb-0'>
           <img
             src={'/images/illustration-features-tab-1.svg'}
             alt='bookmark browser illustration'
-            className='sm:ml-auto sm:w-[70%]'
+            className='md:ml-auto md:w-[75%] mx-auto md:mr-0'
           />
-          <span className='illus-bg sm:before:-left-28 sm:before:top-14'></span>
+          {/* <span className='illus-bg sm:before:-left-28 sm:before:top-14'></span> */}
         </div>
         <div className='sm:w-3/5 sm:mx-auto'>
-          <h1 className='text-2xl sm:text-4xl font-bold text-[--clr-very-dark-blue] mt-16 '>
+          <h1 className='text-2xl md:text-4xl font-bold text-[--clr-very-dark-blue] mt-16 '>
             Bookmark in one click
           </h1>
           <p className='text-[--clr-grayish-blue] mt-4'>
@@ -74,7 +76,7 @@ export default function TabFeature() {
           <Button
             variant={'btnPrimary'}
             size={'lg'}
-            className='mt-8 sm:block hidden'
+            className='mt-8 md:block hidden'
           >
             More Info
           </Button>
@@ -83,15 +85,15 @@ export default function TabFeature() {
       <Tabs.Content
         value='speedy'
         data-test='tab-2-content'
-        className='sm:grid sm:grid-cols-2 sm:content-wrapper'
+        className='md:grid md:grid-cols-2 md:content-wrapper'
       >
         <div className='relative'>
           <img
             src={'/images/illustration-features-tab-2.svg'}
             alt='Bookmark search illustration'
-            className='sm:ml-auto sm:w-[70%]'
+            className='md:ml-auto md:w-[65%] mx-auto md:mr-0'
           />
-          <span className='illus-bg sm:before:-left-28 sm:before:top-8'></span>
+          {/* <span className='illus-bg sm:before:-left-28 sm:before:top-8'></span> */}
         </div>
         <div className='sm:w-3/5 sm:mx-auto'>
           <h1 className='text-2xl sm:text-4xl font-bold text-[--clr-very-dark-blue] mt-16'>
@@ -104,7 +106,7 @@ export default function TabFeature() {
           <Button
             variant={'btnPrimary'}
             size={'lg'}
-            className='mt-8 sm:block hidden'
+            className='mt-8 md:block hidden'
           >
             More Info
           </Button>
@@ -113,15 +115,15 @@ export default function TabFeature() {
       <Tabs.Content
         value='easy'
         data-test='tab-3-content'
-        className='sm:grid sm:grid-cols-2 sm:content-wrapper'
+        className='md:grid md:grid-cols-2 md:content-wrapper'
       >
         <div className='relative'>
           <img
             src={'/images/illustration-features-tab-3.svg'}
             alt='Bookmark sharing illustration'
-            className='sm:ml-auto sm:w-[70%]'
+            className='ml-auto md:w-[65%] mx-auto md:mr-0'
           />
-          <span className='illus-bg sm:before:-left-28 sm:before:top-8'></span>
+          {/* <span className='illus-bg sm:before:-left-28 sm:before:top-8'></span> */}
         </div>
         <div className='sm:w-3/5 sm:mx-auto'>
           <h1 className='text-2xl sm:text-4xl font-bold text-[--clr-very-dark-blue] mt-16'>
@@ -134,7 +136,7 @@ export default function TabFeature() {
           <Button
             variant={'btnPrimary'}
             size={'lg'}
-            className='mt-8 sm:block hidden'
+            className='mt-8 md:block hidden'
           >
             More Info
           </Button>
